851165f34f89ed0364462539fd4219239aa18f91
faiz-hmed <faiz.ahmeds2000@gmail.com>
2023-04-10 16:46:48 +0530

    Added Schema and user routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/DLancer', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userRoutes = require('./routes/userRoutes/index.js');
+
+const app =  express();
+
+app.use(express.json());
+
+// app.use("/api/users", userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+  console.log(`Server started at PORT: ${PORT}`);
+});
