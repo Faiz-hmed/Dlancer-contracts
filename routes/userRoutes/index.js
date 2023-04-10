4c5ca8c185d53f8231c7d1c938dcf79b5af8321d
faiz-hmed <faiz.ahmeds2000@gmail.com>
2023-04-11 02:07:12 +0530

    Completed User request routes

--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -4,24 +4,16 @@ const Models = require('../../Schema/index.js');
 const userModel = Models.Users;
 const projectModel = Models.Projects;
 
-router.get("/", async (res, req) => {           
-    // Users list endpoint
-    const query = req.query;
-    const users = await userModel.find({}).exec();
-
-    return res.status(200).send(users);
-});
-
 router.get("/login/:userid", async (req, res) => {
     // Check for user in DB
     const userId = req.params.userid;
     const registeredUser = await userModel.findOne({walletID: userId}).exec();
 
     if(registeredUser !== null){
-        return res.status(200).send(true);
+        return res.status(200).send({ success: true, message: 'User Registered' });
     }
     else{
-        return res.status(400).send(false);
+        return res.status(400).send({ success: false, message: 'User not registered!' });
     }
 });
 
@@ -44,16 +36,29 @@ router.post("/signup", async (req, res) => {
     }
 });
 
+// TODO : Add code to store certs 
+
+router.get("/", async (res, req) => {           
+    // Users list endpoint
+    const users = await userModel.find({}).exec();
+
+    return res.status(200).send(users);
+});
+
 router.get("/:userid", async (req, res) => {
     // Get user details
     const userId = req.params.userid;
     const user = await userModel.findOne({walletID: userId}).exec();
-    const projects = await projectModel.find({ownerID: userId}).exec();
+    await user.populate(['certs','requests']);
+
+    const project = await projectModel.find({ownerID: userId}).exec();
+    await project.populate('collaborators');
 
     if(user === null){
         return res.status(400).send({ success: false, message: 'User not found!' });
     }
 
-    const userDet = {...user, ...projects};
+    const userDet = {...user, ...project};
     return res.status(200).send(userDet);
 });
+
--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/DLancer', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const userRoutes = require('./routes/userRoutes/index.js');
+const reqRoutes = require('./routes/userRoutes/requests.js');
 
 const app =  express();
 
 app.use(express.json());
 
-// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/req", reqRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
 
 const PORT = process.env.PORT || 5000;
